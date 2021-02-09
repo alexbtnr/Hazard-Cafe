d6198686d0118e9139c4578b61b631afbcbe5607
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-02-09 19:18:13 +0200

    Added the final touches for the website which include a simple JS script for the transparency of the navbar, a smooth scroll behaviour for link clicking, a lightbox effect for which I needed to implement JQuery and last, but not least, a favicon.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Little animation for navbar
+const navbar = document.querySelector('#navbar');
+let scrolled = false;
+
+window.addEventListener('scroll', function () {
+    if(window.pageYOffset > 100) {
+        navbar.classList.remove('top');
+        if (!scrolled) {
+            navbar.style.transform = 'translateY(-70px)';
+        }
+        setTimeout(function () {
+            navbar.style.transform = 'translateY(0)';
+            scrolled = true;
+        }, 200);
+    } else {
+        navbar.classList.add('top');
+        scrolled = false;
+    }
+})
